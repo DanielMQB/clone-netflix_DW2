2ca5c90ac4b959264b26feb16aed714d1ecd6d46
DanielMQB <38981481+DanielMQB@users.noreply.github.com>
2022-11-01 17:04:13 -0300

    Atualizando projeto

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import "./App.css";
-import React, { useState, useEffect } from "react";
-import tmdb from "./tmdb";
+import './App.css'
+import React from 'react'
+import { connect } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import Routes from './Routes'
+import { Template } from './components/MainComponents'
+import Header from './components/partials/Header'
+import Footer from './components/partials/Footer'
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default () => {
-  const [movieList, setMovieList] = useState([]);
-  const [featureData, setFeatureData] = useState(null);
-
-  useEffect(() => {
-    const loadAll = async () => {
-      let list = await tmdb.getHomeList();
-      console.log(list);
-      setMovieList(list);
-
-      // eslint-disable-next-line no-undef
-      let originals = list.filter((i = i.slug === "originals"));
-      let randomChosen = Math.floor(
-        Math.random() * originals[0].items.results.length - 1
-      );
-      let chosen = originals[0].items.results[randomChosen];
-      let chosenInfo = await tmdb.getMovieInfo(chosen.id, "tv");
-      console.log(chosenInfo);
-      setFeatureData(chosenInfo);
-    };
-    loadAll();
-  }, []);
-
+const Page = (props) => {
   return (
-    <div className="page">
-      
-    </div>
-  );
-};
+    <BrowserRouter>
+      <Template>
+        <Header />
+        <Routes />
+        <Footer />
+      </Template>
+    </BrowserRouter>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (Page)
